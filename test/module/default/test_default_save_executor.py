d048a62aac898364ed7c52e28991e9bdd092d76e
Felipe Hernandez <fllamas@travix.com>
2019-11-29 17:15:25 +0000

    feat: save: add evaluation

--- test/module/default/test_default_save_executor.py
+++ test/module/default/test_default_save_executor.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from unittest.mock import patch
 
 from folker.model.data import StageData
 from folker.model.error.variables import VariableReferenceResolutionException
@@ -49,7 +50,8 @@ class TestDefaultSaveExecutor(TestCase):
         self.assertEqual({'save_in': 'value_to_save'}, test_context)
         self.assertEqual({'referenced_value_to_save': 'value_to_save'}, stage_context)
 
-    def test_given_referenced_saves_in_test_context_then_update_test_context(self):
+    @patch('folker.module.default.save_executor.eval', **{'return_value.raiseError.side_effect': Exception()})
+    def test_given_referenced_saves_in_test_context_then_update_test_context(self, eval_mock):
         executor = DefaultSaveExecutor()
 
         stage_data = StageData(
@@ -57,6 +59,8 @@ class TestDefaultSaveExecutor(TestCase):
             name='stage_name',
             save={'save_in': '${referenced_value_to_save}'}
         )
+
+        eval_mock.side_effect = lambda x: exec('raise(Exception(x))')
 
         try:
             executor.execute(stage_data, {'referenced_value_to_save': 'value_to_save'}, {})
@@ -66,3 +70,17 @@ class TestDefaultSaveExecutor(TestCase):
             self.assertEqual('Missing reference in context', ex.error)
             self.assertEqual('Variable reference cannot be resolved', ex.cause)
             self.assertTrue('referenced_value_to_save' in ex.details['reference'])
+
+    def test_given_evaluation_string_then_update_test_context_with_evaluation(self):
+        executor = DefaultSaveExecutor()
+
+        stage_data = StageData(
+            id='default',
+            name='stage_name',
+            save={'save_in': '1 + 1'}
+        )
+
+        test_context, stage_context = executor.execute(stage_data, {}, {})
+
+        self.assertEqual({'save_in': 2}, test_context)
+        self.assertEqual({}, stage_context)
