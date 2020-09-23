5ee1fdad8a9202010d9d547cbcd8e9233718b6a3
felipehernandez <felherlla@gmail.com>
2020-09-23 16:52:54 +0100

    feat: gmail action (#41)

---
+++ folker/module/gmail/schema.py
@@ -0,0 +1,21 @@
+from marshmallow import Schema, fields, post_load
+
+from folker.module.gmail.action import GmailAction
+from folker.module.rest.action import RestAction
+
+
+class GmailActionSchema(Schema):
+    type = fields.String()
+
+    method = fields.String()
+    credentials_path = fields.String()
+    sender = fields.String(data_key='from')
+    recipients = fields.List(cls_or_instance=fields.String, data_key='to')
+    hidden_recipients = fields.List(cls_or_instance=fields.String, data_key='cc')
+    subject = fields.String()
+    text = fields.String()
+    html = fields.String()
+
+    @post_load
+    def make_action(self, data, **kwargs):
+        return GmailAction(**data)
