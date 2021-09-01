c662df8f126b0062177451a22df143ee598b32f0
fllamas <fllamas@travix.com>
2021-09-01 18:37:35 +0100

    feat(version): 2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='folker-test',
-    version='0.1.0',
+    version='2.1.0',
     license='proprietary',
     description='Test and simple tasks automation tool',
 
