c63dcce426b1e723bbea49b1e6df780387a5104f
felipehernandez <felherlla@gmail.com>
2021-11-15 17:02:36 +0000

    feat (timed_action): use time.perf_counter" (#145)
    Use time.perf_counter() instead of time.time() for more accourate measurement

--- tests/decorator/test_timed.py
+++ tests/decorator/test_timed.py
@@ -3,7 +3,7 @@ from folker.model import Context
 
 
 def test_timed_action(mocker):
-    mocker.patch('folker.decorator.timed.time', side_effect=[1, 2])
+    mocker.patch('folker.decorator.timed.time.perf_counter', side_effect=[1, 2])
 
     @timed_action
     def called_method(self, *args, **kargs):
