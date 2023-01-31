9e85d40a78ceeff50b579acf93b9fed8555d17b9
felherlla <felherlla@gmail.com>
2023-01-31 11:55:58 +0000

    refactor(gcp/pubsub) split StageAction into individual classes

--- folker/module/gcp/pubsub/action.py
+++ folker/module/gcp/pubsub/action.py
@@ -34,24 +34,26 @@ class PubSubStageAction(StageAction):
     subscription: str
     ack: bool = None
 
-    def __init__(self,
-                 method: str = None,
-                 host: str = None,
-                 project: str = None,
-                 credentials: str = None,
-                 topic: str = None,
-                 subscription=None,
-                 attributes: dict = None,
-                 message=None,
-                 ack: bool = None,
-                 **kargs) -> None:
+    def __init__(
+        self,
+        method: str = None,
+        host: str = None,
+        project: str = None,
+        credentials: str = None,
+        topic: str = None,
+        subscription=None,
+        attributes: dict = None,
+        message=None,
+        ack: bool = None,
+        **kargs,
+    ) -> None:
         super().__init__()
 
         if method:
             try:
                 self.method = PubSubMethod[method]
             except Exception as ex:
-                raise InvalidSchemaDefinitionException(wrong_fields=['action.method'])
+                raise InvalidSchemaDefinitionException(wrong_fields=["action.method"])
 
         self.host = host
         self.project = project
@@ -68,7 +70,7 @@ class PubSubStageAction(StageAction):
     def __copy__(self):
         return deepcopy(self)
 
-    def __add__(self, enrichment: 'PubSubStageAction'):
+    def __add__(self, enrichment: "PubSubStageAction"):
         result = self.__copy__()
         if isinstance(enrichment, VoidStageAction):
             return result
@@ -92,101 +94,19 @@ class PubSubStageAction(StageAction):
         return result
 
     def mandatory_fields(self):
-        return [
-            'project',
-            'method'
-        ]
+        return ["project", "method"]
 
     def _validate_specific(self):
-        if hasattr(self, 'method') and PubSubMethod.PUBLISH is self.method:
-            if not hasattr(self, 'topic') or not self.topic:
-                self.validation_report.missing_fields.add('action.topic')
-            if not hasattr(self, 'message') or not self.message:
-                self.validation_report.missing_fields.add('action.message')
-        if hasattr(self, 'method') and PubSubMethod.SUBSCRIBE is self.method:
-            if not hasattr(self, 'subscription') or not self.subscription:
-                self.validation_report.missing_fields.add('action.subscription')
+        pass
 
     @loggable_action
     @resolvable_variables
     @timed_action
     def execute(self, logger: TestLogger, context: Context) -> Context:
-        self._authenticate()
-        {
-            PubSubMethod.PUBLISH: self._publish,
-            PubSubMethod.SUBSCRIBE: self._subscribe,
-            PubSubMethod.TOPICS: self._topics,
-            PubSubMethod.SUBSCRIPTIONS: self._subscriptions
-        }.get(self.method)(logger, context)
-
-        return context
-
-    def _publish(self, logger: TestLogger, context: Context):
-        self.publisher = PublisherClient(channel=grpc.insecure_channel(target=self.host)) \
-            if self.host \
-            else PublisherClient()
-
-        topic_path = self.publisher.topic_path(self.project, self.topic)
-        attributes = self.attributes if self.attributes else {}
-
-        self._log_debug(logger, topic=topic_path, attributes=attributes, message=str(self.message))
-
-        message = self.message \
-            if isinstance(self.message, (bytes, bytearray)) \
-            else self.message.encode()
-        future = self.publisher.publish(topic=topic_path, data=message, **attributes)
-
-        context.save_on_stage('message_id', future.result())
-
-    def _subscribe(self, logger: TestLogger, context: Context):
-        self.subscriber = SubscriberClient(channel=grpc.insecure_channel(target=self.host)) \
-            if self.host \
-            else SubscriberClient()
-
-        subscription_path = self.subscriber.subscription_path(self.project, self.subscription)
-
-        self._log_debug(logger, subscription=subscription_path, ack=bool(self.ack))
-        response = self.subscriber.pull(subscription=subscription_path,
-                                        max_messages=1)
-
-        for message in response.received_messages:
-            context.save_on_stage('ack_id', message.ack_id)
-            context.save_on_stage('message_id', message.message.message_id)
-            context.save_on_stage('publish_time', message.message.publish_time)
-            context.save_on_stage('attributes', message.message.attributes)
-            context.save_on_stage('message_content', message.message.data.decode('UTF-8'))
-
-            if self.ack:
-                self.subscriber.acknowledge(subscription=subscription_path,
-                                            ack_ids=[message.ack_id])
-
-    def _topics(self, logger: TestLogger, context: Context):
-        self.publisher = PublisherClient(channel=grpc.insecure_channel(target=self.host)) \
-            if self.host \
-            else PublisherClient()
-
-        project_path = f'projects/{self.project}'
-        topics = self.publisher.list_topics(project=project_path)
-
-        topic_prefix = project_path + '/topics/'
-        prefix_len = len(topic_prefix)
-        context.save_on_stage('topics', [topic.name[prefix_len:] for topic in topics])
-
-    def _subscriptions(self, logger: TestLogger, context: Context):
-        self.subscriber = SubscriberClient(channel=grpc.insecure_channel(target=self.host)) \
-            if self.host \
-            else SubscriberClient()
-
-        project_path = f'projects/{self.project}'
-        subscriptions = self.subscriber.list_subscriptions(project=project_path)
-
-        subscription_prefix = project_path + '/subscriptions/'
-        context.save_on_stage('subscriptions',
-                              [subscription.name[len(subscription_prefix):]
-                               for subscription in subscriptions])
+        pass
 
     def _authenticate(self):
-        credentials_path = os.getcwd() + '/credentials/gcp/gcp-credentials.json'
+        credentials_path = os.getcwd() + "/credentials/gcp/gcp-credentials.json"
         if os.path.exists(credentials_path):
             self.credentials_path = credentials_path
             os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = self.credentials_path
@@ -194,3 +114,199 @@ class PubSubStageAction(StageAction):
     @staticmethod
     def _log_debug(logger: TestLogger, **parameters):
         logger.action_debug(json.dumps(parameters))
+
+
+class PubSubStagePublishAction(PubSubStageAction):
+    method: PubSubMethod
+
+    host: str
+    project: str
+    credentials_path: str
+
+    topic: str
+    attributes: dict
+    message: str
+    subscription: str
+    ack: bool = None
+
+    def __init__(self, **fields):
+        super().__init__(**fields)
+
+    def _validate_specific(self):
+        if not hasattr(self, "topic") or not self.topic:
+            self.validation_report.missing_fields.add("action.topic")
+        if not hasattr(self, "message") or not self.message:
+            self.validation_report.missing_fields.add("action.message")
+
+    @loggable_action
+    @resolvable_variables
+    @timed_action
+    def execute(self, logger: TestLogger, context: Context) -> Context:
+        self._authenticate()
+
+        self.publisher = (
+            PublisherClient(channel=grpc.insecure_channel(target=self.host))
+            if self.host
+            else PublisherClient()
+        )
+
+        topic_path = self.publisher.topic_path(self.project, self.topic)
+        attributes = self.attributes if self.attributes else {}
+
+        self._log_debug(
+            logger, topic=topic_path, attributes=attributes, message=str(self.message)
+        )
+
+        message = (
+            self.message
+            if isinstance(self.message, (bytes, bytearray))
+            else self.message.encode()
+        )
+        future = self.publisher.publish(topic=topic_path, data=message, **attributes)
+
+        context.save_on_stage("message_id", future.result())
+
+        return context
+
+
+class PubSubStageSubscribeAction(PubSubStageAction):
+    method: PubSubMethod
+
+    host: str
+    project: str
+    credentials_path: str
+
+    topic: str
+    attributes: dict
+    message: str
+    subscription: str
+    ack: bool = None
+
+    def __init__(self, **fields):
+        super().__init__(**fields)
+
+    def _validate_specific(self):
+        if not hasattr(self, "subscription") or not self.subscription:
+            self.validation_report.missing_fields.add("action.subscription")
+
+    @loggable_action
+    @resolvable_variables
+    @timed_action
+    def execute(self, logger: TestLogger, context: Context) -> Context:
+        self._authenticate()
+
+        self.subscriber = (
+            SubscriberClient(channel=grpc.insecure_channel(target=self.host))
+            if self.host
+            else SubscriberClient()
+        )
+
+        subscription_path = self.subscriber.subscription_path(
+            self.project, self.subscription
+        )
+
+        self._log_debug(logger, subscription=subscription_path, ack=bool(self.ack))
+        response = self.subscriber.pull(subscription=subscription_path, max_messages=1)
+
+        for message in response.received_messages:
+            context.save_on_stage("ack_id", message.ack_id)
+            context.save_on_stage("message_id", message.message.message_id)
+            context.save_on_stage("publish_time", message.message.publish_time)
+            context.save_on_stage("attributes", message.message.attributes)
+            context.save_on_stage(
+                "message_content", message.message.data.decode("UTF-8")
+            )
+
+            if self.ack:
+                self.subscriber.acknowledge(
+                    subscription=subscription_path, ack_ids=[message.ack_id]
+                )
+
+        return context
+
+
+class PubSubStageTopicsAction(PubSubStageAction):
+    method: PubSubMethod
+
+    host: str
+    project: str
+    credentials_path: str
+
+    topic: str
+    attributes: dict
+    message: str
+    subscription: str
+    ack: bool = None
+
+    def __init__(self, **fields):
+        super().__init__(**fields)
+
+    def _validate_specific(self):
+        pass
+
+    @loggable_action
+    @resolvable_variables
+    @timed_action
+    def execute(self, logger: TestLogger, context: Context) -> Context:
+        self._authenticate()
+
+        self.publisher = (
+            PublisherClient(channel=grpc.insecure_channel(target=self.host))
+            if self.host
+            else PublisherClient()
+        )
+
+        project_path = f"projects/{self.project}"
+        topics = self.publisher.list_topics(project=project_path)
+
+        topic_prefix = project_path + "/topics/"
+        prefix_len = len(topic_prefix)
+        context.save_on_stage("topics", [topic.name[prefix_len:] for topic in topics])
+
+        return context
+
+
+class PubSubStageSubscriptionsAction(PubSubStageAction):
+    method: PubSubMethod
+
+    host: str
+    project: str
+    credentials_path: str
+
+    topic: str
+    attributes: dict
+    message: str
+    subscription: str
+    ack: bool = None
+
+    def __init__(self, **fields):
+        super().__init__(**fields)
+
+    def _validate_specific(self):
+        pass
+
+    @loggable_action
+    @resolvable_variables
+    @timed_action
+    def execute(self, logger: TestLogger, context: Context) -> Context:
+        self._authenticate()
+
+        self.subscriber = (
+            SubscriberClient(channel=grpc.insecure_channel(target=self.host))
+            if self.host
+            else SubscriberClient()
+        )
+
+        project_path = f"projects/{self.project}"
+        subscriptions = self.subscriber.list_subscriptions(project=project_path)
+
+        subscription_prefix = project_path + "/subscriptions/"
+        context.save_on_stage(
+            "subscriptions",
+            [
+                subscription.name[len(subscription_prefix) :]
+                for subscription in subscriptions
+            ],
+        )
+
+        return context
